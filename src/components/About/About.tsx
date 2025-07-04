3a3eda88b000685e4ec98eeb5e215335ef0e6e8d
Muhammad Huzaifa Khan <huzaifa.khan@chaptervitamins.com>
2025-07-04 06:27:52 +0530

    Initial project setup with essential files including package.json, .gitignore, and README.md. Added components for About, Contact, Footer, Header, Hero, Main, and Project sections. Included global styles and various assets such as images and SVG icons. Configured TypeScript and package-lock for dependency management.

---
+++ src/components/About/About.tsx
@@ -0,0 +1,101 @@
+import { Container } from "./styles";
+import HuzaifaKhan from "../../assets/Huzaifa_khan.jpg";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            Hi there! I'm Muhammad Huzaifa Khan, a passionate Full Stack Developer with over four years of experience in designing and building dynamic, scalable, and efficient web and mobile applications. I specialize in using modern technologies to create seamless user experiences and high-performance applications.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          My expertise spans a wide range of technologies, including PHP, Node.js, React.js, Next.js, Angular, TypeScript, React Native, and Python. I enjoy solving complex technical challenges and optimizing applications for better performance and scalability. Whether it's frontend development for a sleek UI/UX experience or backend architecture for robust server-side logic, I ensure every project is executed with precision and efficiency.
+          </p>
+
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+          Beyond coding, I am always eager to learn and explore new tools and frameworks to enhance my development skills. I believe in writing clean, maintainable code and following industry best practices to deliver top-notch solutions. Let's connect and bring innovative ideas to life together!
+            </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+              <img src={wordpress} alt="Wordpress" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={shopify} alt="shopify" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
+              <img src={vueIcon} alt="Vue" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={HuzaifaKhan} alt="Muhammad Huzaifa Khan" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,62 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="https://themuhammadhuzaifakhan.tech/" className="logo">
+        {/* <span>www.themuhammadhuzaifa</span>
+        <span>khan.tech</span> */}
+        <span>Home</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/themuhammadhuzaifakhan1211"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/huzaifakhan1225"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=%2B917007174034&text=Hello+Huzaifa,+I+am+from+your+website&app_absent=0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href="https://t.me/+917007174034"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/iitian_abu_zunnoorain?igsh=MWx5czQxdHFnOGhxMA%3D%3D&utm_source=qr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
